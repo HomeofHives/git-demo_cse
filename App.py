8beb4e24ae6139d229768e35a6465f448ba2a80c
HomeofHives <150236153+HomeofHives@users.noreply.github.com>
2026-02-06 11:03:37 +0530

    Implement basic arithmetic functions in App.py

---
+++ App.py
@@ -0,0 +1,20 @@
+def add(a,b):
+  return a+b;
+
+def sub(a,b):
+  return a-b;
+
+def mul(a,b):
+  return a*b;
+
+def div(a,b):
+  if b==0:
+    print("ERROR : Division By Zero")
+    return
+  return a/b;
+
+if __name__ == "__main__":
+  print(f"2 + 3 : {add(2,3)}")
+  print(f"2 - 3 : {sub(2,3)}")
+  print(f"2 * 3 : {mul(2,3)}")
+  print(f"3 / 3 : {div(2,3)}")
